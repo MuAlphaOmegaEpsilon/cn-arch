efd915f5aa73609e1c7f1253f94c070749f4fb9a
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2025-04-25 22:59:16 +0200

    Update deploy script and add new favicons

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,14 +7,16 @@ find docs/ -name "*.html" -exec rm {} \;
 
 rm -r .preprocessed
 mkdir -p .preprocessed
-# for CSS_FILE in $(ls | grep ".css"); do
-	# yui-compressor "${CSS_FILE}" --output .preprocessed/"${CSS_FILE}"
-# done
+for CSS_FILE in $(ls | grep ".css"); do
+	yui-compressor "${CSS_FILE}" --output docs/"${CSS_FILE}"
+done
 
 find . -maxdepth 1 -name "*.css" -exec ln -s .{} .preprocessed/ \;
-m4 index.html | inliner --noimages --nocompress --nosvg > docs/index.html
+m4 index.html > docs/index.html
+# m4 index.html | inliner --noimages --nocompress --nosvg > docs/index.html
 
-m4 -DFILE=claudia-negrini.html project-template.html | inliner --noimages --nocompress --nosvg > docs/claudia-negrini.html
+m4 -DFILE=claudia-negrini.html project-template.html > docs/claudia-negrini.html
+# m4 -DFILE=claudia-negrini.html project-template.html | inliner --noimages --nocompress --nosvg > docs/claudia-negrini.html
 
 for PROJECT_FILE in $(find . -maxdepth 1 -name "project-*.html" | grep -v project-template.html); do
 	m4 -DFILE=${PROJECT_FILE} project-template.html | inliner --noimages --nocompress --nosvg > docs/$(echo ${PROJECT_FILE} | colrm 1 10)
